9bd16d302ac812cedc0db5d1fb06924044ddc78b
mikhaillav <59438110+mikhaillav@users.noreply.github.com>
2022-01-18 22:12:02 +0300

    add config, remove CPU load check

--- source/LiteAdminPanel.cpp
+++ source/LiteAdminPanel.cpp
@@ -1,29 +1,58 @@
 #include <iostream>
 #include <string>
+#include <direct.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
 #include <windows.h>
+#include <fstream>
+#include "dir.h"
 #include <cmath> //hahaha, yes i know about it)
 using namespace std;
 
 string output;
+char buff[50];
+string buf;
+string server_name;
+int yes = 1;
+
 
 void help() {
     cout << "| " << "list of commands:\n";
     cout << "| " << "help(?) - show this text\n";
-    cout << "| " << "exit - exit program\n";
     cout << "| " << "info - show info about LA\n";
-    cout << "| " << "gpu - approximate CPU load\n";
+    cout << "| " << "setup - change or setup config\n";
+    cout << "| " << "exit - exit program\n";
 }
 
 void info() {
     cout << "| LiteAdminPanel version: beta-0.0.3\n";
     cout << "| github: https://github.com/orgs/LiteAdminPanel/\n";
     cout << "| discord: prorok#1433, VinkyV#7660\n";
-    cout << "| LiteAdminPanel by VinkW, prorok & c++\n\n";
+    cout << "| LiteAdminPanel by VinkW, prorok & c++\n";
 }
 
-int GetCpuUsage()
+
+void config() {
+    system("mkdir LiteAdmin");
+    ofstream fout(".\\LiteAdmin\\config.txt");
+    fout << "no" << endl;
+    fout << "server";
+    fout.close();
+}
+
+void setup() {
+    cout << "type name of your server:\n";
+    cin >> server_name;
+    ofstream fout(".\\LiteAdmin\\config.txt");
+    fout << "yes" << endl;
+    fout << server_name;
+    fout.close();
+    cout << "[info] " << "the config was successfully changed\n";
+    yes = 0;
+}
+
+/*int GetCpuUsage()
 {
     static ULARGE_INTEGER TimeIdle, TimeKernel, TimeUser;
     FILETIME Idle, Kernel, User;
@@ -40,12 +69,20 @@ int GetCpuUsage()
     TimeUser.QuadPart = uUser.QuadPart;
     TimeKernel.QuadPart = uKernel.QuadPart;
     return(static_cast<int>(t) + (static_cast<int>(t) / 2));
-}
+}*/
 
 
 void error(int code) {
+    if (code == -1) {
+        //cout << "[warn] " << "unknown command, type 'help' for help\n";
+    }
     if (code == 0) {
         cout << "[warn] " << "unknown command, type 'help' for help\n";
+    }
+    else if (code == 1) {
+        cout << "[warn] " << "error when open config\n";
+        system("pause");
+        output = "exit";
     }
     else {
         cout << "[warn] " << "something was wrong and we can't find problem pls say us about this\n";
@@ -62,8 +99,27 @@ int main()
     cout << "[info] " << "LiteAdminPanel start type 'help' to get list of commands\n";
     cout << "[info] " << "If you have any issues pls say us about it(our contacts in /info)\n\n";
 
+    ifstream fin(".\\LiteAdmin\\config.txt");
+    if (!fin.is_open()) {
+        config();
+    }
+
+    fin.getline(buff, 50);
+    buf = buff;
+    if (!(buf == "yes")) {
+        cout << "[info] " << "LiteAdminPanel don't config now, type '/setup' to fix it\n";
+    }
+    else {
+        fin.getline(buff, 50);
+        buf = buff;
+        cout << "[info] " << "LiteAdminPanel load config for '" << buf << "'\n";
+    }
+
     while (output != "exit") {
-        cout << "> ";
+        if (yes == 1) {
+            cout << "> ";
+        }
+        yes = 1;
         getline(cin, output);
         if (output == "exit") {
             system("pause");
@@ -77,8 +133,11 @@ int main()
         else if (output == "info") {
             info();
         }
-        else if (output == "gpu") {
-            cout << "[info]" << "GPU load:" << GetCpuUsage() << "%\n";
+        else if (output == "setup") {
+            setup();
+        }
+        else if (output == "") {
+
         }
         else {
             error(0);
